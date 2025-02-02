32db0f07290e8ecb071628a45958b92981115c0a
Ξmon <65068418+2dvoid@users.noreply.github.com>
2025-02-03 00:02:50 +0600

    Update Frequency.c to Iterative

--- Binary_Search/Frequency.c
+++ Binary_Search/Frequency.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
 
-int firstOccurrence (int *arr, int low, int high, int target);
-int lastOccurrence (int *arr, int low, int high, int target);
+int findLast(int *arr, int n, int target);
+int findFirst(int *arr, int n, int target);
 
 int main () {
-    int arr[]={10,20,30,40,50,60,70,70,70,70,70,70,80,90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 70;
-    int first = firstOccurrence(arr, 0, n-1, target);
-    if (first==-1) printf("Data Not Found!\n");
+    int arr[]={5,5,5,10,10,40,60,70,70,70,70,80};
+    int n = sizeof (arr) / sizeof (arr[0]);
+    int target =10;
+    int first = findFirst(arr, n, target);
+    if (first==-1) printf("Data is not present\n");
     else {
-        int last = lastOccurrence(arr, 0, n-1, target);
+        int last = findLast(arr, n, target);
         int frequency = last - first + 1;
-        printf("Data Appears %d times.\n", frequency);
+        printf("Number of Occurrence is %d\n", frequency);
     }
-    
-
 }
 
-int firstOccurrence (int *arr, int low, int high, int target) {
-    if (low > high) return -1;
-    int mid = low + (high-low)/2;
-    if (arr[mid]==target) {
-        if (mid == 0 || arr[mid-1]!=target) return mid;
-        else return firstOccurrence(arr, low, mid-1, target);
+int findFirst(int *arr, int n, int target){
+    int low=0, high=n-1;
+    while (low<=high) {
+        int mid = low + (high-low)/2;
+        if (arr[mid]==target) {
+            if (mid==0 || arr[mid-1]!=target) return mid;
+            else high = mid-1;
+        }
+        else if (target > arr[mid]) low = mid+1;
+        else high = mid-1;
     }
-    else if (target < arr[mid]) return firstOccurrence(arr, low, mid-1, target);
-    else return firstOccurrence(arr, mid+1, high, target);
+    return -1;
 }
 
-int lastOccurrence(int *arr, int low, int high, int target) {
-    if (low > high) return -1;
-    int mid = low + (high-low)/2;
-    if (arr[mid]==target) {
-        if (mid==high || arr[mid+1]!= target) return mid;
-        else return lastOccurrence(arr, mid+1, high, target);
+int findLast(int *arr, int n, int target){
+    int low = 0, high =  n-1;
+    while (low <= high) {
+        int  mid = low + (high-low)/2;
+        if (arr[mid]==target) {
+            if (mid==n-1 || arr[mid+1]!=target) return mid;
+            else low = mid+1;
+        }
+        else if (target > arr[mid]) low = mid+1;
+        else high = mid-1;
     }
-    else if (target < arr[mid]) return lastOccurrence(arr, low, mid-1, target);
-    else return lastOccurrence(arr, mid+1, high, target);
+    return -1;
 }
